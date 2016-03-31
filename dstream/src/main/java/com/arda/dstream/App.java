6b56a87f5dd4ca88a8b52019d589812eb9ea687b
asad-kazmi <asad.kazmi@qbxnet.com>
2016-03-31 11:23:03 +0500

    Spark-Java Stream working

--- dstream/src/main/java/com/arda/dstream/App.java
+++ dstream/src/main/java/com/arda/dstream/App.java
@@ -1,14 +1,45 @@
 package com.arda.dstream;
 
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App implements MqttCallback {
+	public static void main(String[] args) {
+		try {
+			MqttClient client = new MqttClient("tcp://ec2-54-93-53-98.eu-central-1.compute.amazonaws.com:1883", "Listening");
+			App app = new App();
+			client.connect();
+			client.setCallback(app);
+			client.subscribe("connectavo.devices.4000002707881.191238");
+//			client.sub
+			
+			// MqttMessage message = new MqttMessage();
+			// message.setPayload("A single message from my computer
+			// fff".getBytes());
+			// client.publish("foo", message);
+		} catch (Exception exp) {
+			exp.printStackTrace();
+		}
+	}
+
+	public void connectionLost(Throwable arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deliveryComplete(IMqttDeliveryToken arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void messageArrived(String arg0, MqttMessage arg1) throws Exception {
+		System.out.println(arg0+" "+arg1.toString());
+
+	}
 }
--- dstream/src/main/java/com/arda/dstream/JavaMQTTWordCount.java
+++ dstream/src/main/java/com/arda/dstream/JavaMQTTWordCount.java
@@ -20,29 +20,33 @@ package com.arda.dstream;
 import java.util.regex.Pattern;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.StorageLevels;
 import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.storage.StorageLevel;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.mqtt.MQTTUtils;
 
-import scala.Tuple2;
-
 import com.google.common.collect.Lists;
 
+import scala.Tuple2;
+
 /**
- * Consumes messages from one or more topics in Kafka and does wordcount.
+ * com.arda.dstream.JavaMQTTWordCount Consumes
+ * messages from one or more topics in Kafka and does wordcount.
  *
- * Usage: JavaKafkaWordCount <zkQuorum> <group> <topics> <numThreads> <zkQuorum>
- * is a list of one or more zookeeper servers that make quorum <group> is the
- * name of kafka consumer group <topics> is a list of one or more kafka topics
- * to consume from <numThreads> is the number of threads the kafka consumer
- * should use
+ * Usage: JavaKafkaWordCount
+ * <zkQuorum> <group> <topics> <numThreads> <zkQuorum> is a list of one or more
+ * zookeeper servers that make quorum <group> is the name of kafka consumer
+ * group <topics> is a list of one or more kafka topics to consume from
+ * <numThreads> is the number of threads the kafka consumer should use
  *
  * To run this example: `$ bin/run-example
  * org.apache.spark.examples.streaming.JavaKafkaWordCount zoo01,zoo02, \ zoo03
@@ -56,54 +60,53 @@ public final class JavaMQTTWordCount {
 	}
 
 	public static void main(String[] args) {
-	
+
 		// StreamingExamples.setStreamingLogLevels();
-		SparkConf sparkConf = new SparkConf().setAppName("JavaMQTTWordCount");
-		sparkConf.setMaster("spark://ec2-54-169-113-223.ap-southeast-1.compute.amazonaws.com:7077");
+		SparkConf sparkConf = new SparkConf().setAppName("JavaMQTTWordCount").setMaster("local[4]").
+				set("spark.driver.allowMultipleContexts", "true");
+		//sparkConf.setMaster("spark://asadkazmi-Inspiron-5548:7077");
+//		sparkConf.set("spark.driver.allowMultipleContexts", "true");
+			
 		// Create the context with 2 seconds batch size
-		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf,
-				new Duration(2000));
-
+		JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, new Duration(2000));
+//		JavaReceiverInputDStream<String> messages = jssc.socketTextStream("ec2-54-93-53-98.eu-central-1.compute.amazonaws.com", 61616);
 		
-		String brokerUrl = "mqtt://localhost:61616";
-		String topic = "connectavo.devices";
 		
-
-		JavaReceiverInputDStream<String> messages = MQTTUtils
-				.createStream(jssc, brokerUrl, topic);
-
+		String brokerUrl = "tcp://ec2-54-93-53-98.eu-central-1.compute.amazonaws.com:1883";
+		String topic = "connectavo.devices.4000002707881.191238";
 		
-		JavaDStream<String> lines = messages.map(new Function<String, String>() {
+		JavaReceiverInputDStream<String> messages = MQTTUtils.createStream(jssc, brokerUrl, topic,StorageLevels.MEMORY_AND_DISK_SER);
+	
+/*		JavaDStream<String> lines = messages.map(new Function<String, String>() {
 
 			public String call(String arg0) throws Exception {
-				
 				return arg0;
 			}
 		});
-		
-		
-		JavaDStream<String> words = lines
-				.flatMap(new FlatMapFunction<String, String>() {
-		
-					public Iterable<String> call(String x) {
-						return Lists.newArrayList(SPACE.split(x));
-					}
-				});
 
-		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
-				new PairFunction<String, String, Integer>() {
-	
-					public Tuple2<String, Integer> call(String s) {
-						return new Tuple2<String, Integer>(s, 1);
-					}
-				}).reduceByKey(new Function2<Integer, Integer, Integer>() {
-		
-			public Integer call(Integer i1, Integer i2) {
-				return i1 + i2;
+		JavaDStream<String> words = messages.flatMap(new FlatMapFunction<String, String>() {
+			@Override
+			public Iterable<String> call(String x) {
+				System.out.println(x);
+				return Lists.newArrayList(SPACE.split(x));
 			}
 		});
 
-		wordCounts.print();
+		JavaPairDStream<String, Integer> wordCounts = words.mapToPair(new PairFunction<String, String, Integer>() {
+
+			public Tuple2<String, Integer> call(String s) {
+				return new Tuple2<String, Integer>(s, 1);
+			}
+		}).reduceByKey(new Function2<Integer, Integer, Integer>() {
+
+			public Integer call(Integer i1, Integer i2) {
+
+				return i1 + i2;
+			}
+		});*/
+
+		messages.print();
+		//messages.dstream().log();
 		jssc.start();
 		jssc.awaitTermination();
 	}
